0b8d6cd4e1e41b13d0b96ef0bc757effd22303f2
a <a>
2023-10-01 19:16:33 -0300

    first commit

---
+++ padon/src/main/java/br/com/padon/application/PadonApplication.java
@@ -0,0 +1,13 @@
+package br.com.padon.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PadonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PadonApplication.class, args);
+	}
+
+}
---
+++ padon/src/test/java/br/com/padon/application/PadonApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.padon.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PadonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
