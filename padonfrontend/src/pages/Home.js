a8bea1cba4ab5066bfd56eb1bb3f160b782efd78
elip140 <epcotrim@gmail.com>
2023-10-01 23:04:44 -0300

    a

---
+++ padonfrontend/src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from '../App';
+import reportWebVitals from '../reportWebVitals';
+
+export default function Home() {
+  return (
+    <p>Teste Home</p>
+  );
+}
