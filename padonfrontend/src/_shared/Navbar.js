2f4273947cd595d687bfb4aae3df73050ac4a524
elip140 <epcotrim@gmail.com>
2023-10-08 14:25:30 -0300

    Create Produto Basico e ROUTERS

--- padonfrontend/src/_shared/Navbar.js
+++ padonfrontend/src/_shared/Navbar.js
@@ -1,19 +1,31 @@
 import { Link } from "react-router-dom";
 import Button from 'react-bootstrap/Button';
 
-function Navbar({ nome="" }) {
+function Navbar() {
   return (
     
     <div className="App">
-        <nav class="navbar navbar-dark bg-dark">
-            <div class="container-fluid">
+        <nav className="navbar navbar-dark bg-dark">
+            <div className="container-fluid">
                 <Link to="/">
                     <Button variant="link" className="navbar-brand"> 
                         Padon
                     </Button>
                 </Link>
+                <Link to="/Produtos/Create">
+                    <Button variant="link" className="navbar-brand"> 
+                        Produto
+                    </Button>
+                </Link>
             </div>
         </nav>
+    </div>
+  );
+}
+
+export default Navbar;
+
+/*
         <Link to="/">
             <button className="btn btn-primary" variant="outlined">
                 Home
@@ -26,11 +38,7 @@ function Navbar({ nome="" }) {
         </Link>
         <Link to="/Pagina">
             <button variant="outlined">
-                Teste - {nome}
+                Teste
             </button>
         </Link>
-    </div>
-  );
-}
-
-export default Navbar;
+*/ 
---
+++ padonfrontend/src/pages/Produtos/index.js
@@ -0,0 +1,3 @@
+export { default as ProdutoCreate } from './ProdutoCreate';
+export { default as ProdutoIndex } from './ProdutoIndex';
+export { default as ProdutoDelete } from './ProdutoDelete';
