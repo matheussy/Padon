3d53febf3198c1b59789e7508c0da73f82f36a9c
SeEhLoko <minedoido55@gmail.com>
2023-10-08 16:20:39 -0300

    Deu bom com os produtos

--- padonfrontend/src/App.js
+++ padonfrontend/src/App.js
@@ -1,13 +1,11 @@
 import './style/App.css';
 import Navbar from './_shared/Navbar';
 import Main from './Main';
-import Header from './_shared/Header';
 import Footer from './_shared/Footer';
 
 function App() {
   return (
     <div className="App">
-      <Header />
       <Navbar />
       <Main />
       <Footer/>
--- padonfrontend/src/pages/Produtos/ProdutoCreate.js
+++ padonfrontend/src/pages/Produtos/ProdutoCreate.js
@@ -1,21 +1,23 @@
 import React from 'react';
-import './Style.css';
+import './style.css';
+
+
 
 export default function ProdutosCreate() {
   return (
     <div>
       <div className="container Cadastro_Produtos">
         <form>
-          <ul className="nav nav-tabs" id="myTabs" role="tablist">
-            <li className="nav-item">
-              <a className="nav-link active" id="tab1-tab" data-toggle="tab" href="#tab1" role="tab" aria-controls="tab1" aria-selected="true">Produto</a>
+          <ul className="nav nav-tabs" id="myTab" role="tablist">
+            <li className="nav-item" role="presentation">
+              <button className="nav-link active" id="produto-tab" data-bs-toggle="tab" data-bs-target="#produto" type="button" role="tab" aria-controls="produto" aria-selected="true">Produtos</button>
             </li>
-            <li className="nav-item">
-              <a className="nav-link" id="tab2-tab" data-toggle="tab" href="#tab2" role="tab" aria-controls="tab2" aria-selected="false">Estoque</a>
+            <li className="nav-item" role="presentation">
+              <button className="nav-link" id="estoque-tab" data-bs-toggle="tab" data-bs-target="#estoque" type="button" role="tab" aria-controls="estoque" aria-selected="false">Estoque</button>
             </li>
           </ul>
-          <div className="tab-content" id="myTabsContent">
-            <div className="tab-pane fade show active" id="tab1" role="tabpanel" aria-labelledby="tab1-tab">
+          <div className="tab-content">
+            <div className="tab-pane active" id="produto" role="tabpanel" aria-labelledby="produto-tab">
               <div className="mb-3">
                 <div className="row">
                   <div className="col-2">
@@ -24,15 +26,15 @@ export default function ProdutosCreate() {
                   </div>
                   <div className="col-2">
                     <label htmlFor="UnidadeVenda" className="form-label">Unidade de venda: </label>
-                    <select className="form-select form-select" aria-label=".form-select-sm example">
-                      <option selected />
+                    <select defaultValue="" className="form-select form-select" aria-label=".form-select-sm example">
+                      <option value="" />
                       <option value={1}>UN</option>
                       <option value={2}>KG</option>
                     </select>
                   </div>
                   <div className="col-6">
-                    <label htmlFor="UnidadeVenda" className="form-label">Código de Barras: </label>
-                    <input type="number" className="form-control" id="IdProduto" name="IdProduto" requiredplaceholder="ID do produto...." />
+                    <label htmlFor="CodBarra" className="form-label">Código de Barras: </label>
+                    <input type="number" className="form-control" id="CodBarra" name="IdProduto" requiredplaceholder="ID do produto...." />
                   </div>
                 </div>
               </div>
@@ -43,9 +45,9 @@ export default function ProdutosCreate() {
                     <input type="text" className="form-control" id="nomeProduto" name="nomeProduto" required placeholder="Digite o nome do produto..." />
                   </div>
                   <div className="col-3">
-                    <label htmlFor="nomeProduto" className="form-label">Categoria:</label>
-                    <select className="form-select form-select" aria-label=".form-select-sm example">
-                      <option selected />
+                    <label htmlFor="Categoria" className="form-label">Categoria:</label>
+                    <select defaultValue="" className="form-select form-select" aria-label=".form-select-sm example">
+                      <option value="" />
                       <option value={1}>1</option>
                       <option value={2}>2</option>
                     </select>
@@ -55,8 +57,8 @@ export default function ProdutosCreate() {
               <div className="mb-3">
                 <div className="row">
                   <div className="col-4">
-                    <label htmlFor="nomeProduto" className="form-label">Fabricante:</label>
-                    <input type="text" className="form-control" id="nomeProduto" name="nomeProduto" required placeholder="Digite a marca do produto..." />
+                    <label htmlFor="nomeFabricante" className="form-label">Fabricante:</label>
+                    <input type="text" className="form-control" id="nomeFabricante" name="nomeProduto" required placeholder="Digite a marca do produto..." />
                   </div>
                 </div>
               </div>
@@ -71,26 +73,26 @@ export default function ProdutosCreate() {
               <div className="row">
                 <div className="col-2">
                   <div className="mb-3">
-                    <label htmlFor="preco" className="form-label">Preço por unidade:</label>
-                    <input type="text" className="form-control col-sm-6" id="preco" name="preco" required defaultValue="0,00" />
+                    <label htmlFor="precoUN" className="form-label">Preço por unidade:</label>
+                    <input type="text" className="form-control col-sm-6" id="precoUN" name="precoUN" required defaultValue="0,00" />
                   </div>
                 </div>
                 <div className="col-2">
                   <div className="mb-3">
-                    <label htmlFor="preco" className="form-label">Preço por quilo:</label>
-                    <input type="text" className="form-control col-sm-6" id="preco" name="preco" required defaultValue="0,00" />
+                    <label htmlFor="precoKG" className="form-label">Preço por quilo:</label>
+                    <input type="text" className="form-control col-sm-6" id="precoKG" name="precoKG" required defaultValue="0,00" />
                   </div>
                 </div>
               </div>
               <div className="mb-3">
                 <div className="form-check">
-                  <input className="form-check-input" type="checkbox" defaultValue id="flexCheckDefault" />
+                  <input className="form-check-input" type="checkbox" defaultValue id="flexCheckDefaultEstoque" />
                   <label className="form-check-label" htmlFor="flexCheckDefault">
                     Estoque
                   </label>
                 </div>
                 <div className="form-check">
-                  <input className="form-check-input" type="checkbox" defaultValue id="flexCheckDefault" />
+                  <input className="form-check-input" type="checkbox" defaultValue id="flexCheckDefaultInativo" />
                   <label className="form-check-label" htmlFor="flexCheckDefault">
                     Inativo
                   </label>
@@ -98,8 +100,9 @@ export default function ProdutosCreate() {
               </div>
               <button type="submit" className="btn btn-primary">Cadastrar Produto</button>
             </div>
-            <div className="tab-pane fade" id="tab2" role="tabpanel" aria-labelledby="tab2-tab">
+            <div className="tab-pane" id="estoque" role="tabpanel" aria-labelledby="estoque-tab">
               <div className="mb-3">
+                <p />
                 <label htmlFor="nomeProduto" className="form-label">Nome do Produto</label>
                 <input type="text" className="form-control" id="nomeProduto" name="nomeProduto" required />
               </div>
