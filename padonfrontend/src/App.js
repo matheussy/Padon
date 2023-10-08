bd86e0d9a1c33f66469e48f9e29394d69812cd9d
SeEhLoko <minedoido55@gmail.com>
2023-10-08 18:06:46 -0300

    Mudança de novo kk

--- padonfrontend/src/App.js
+++ padonfrontend/src/App.js
@@ -2,11 +2,13 @@ import './style/App.css';
 import Navbar from './_shared/Navbar';
 import Main from './Main';
 import Footer from './_shared/Footer';
+import Header from './_shared/Header';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Header />
       <Main />
       <Footer/>
     </div>
--- padonfrontend/src/_shared/Header.js
+++ padonfrontend/src/_shared/Header.js
@@ -2,7 +2,7 @@
 function Header() {
   return (
     <div>
-      
+          <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"></link>
     </div>
   );
 }
