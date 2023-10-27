4d741a1155212a1b39f347f3af5fdc081f88cd0d
Matheus Oliveira <matheus.oliveira@sentry.com.br>
2023-10-27 10:21:49 -0300

    testezinho

--- backend/src/main/java/br/com/padon/application/controllers/Controller.java
+++ backend/src/main/java/br/com/padon/application/controllers/Controller.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class Controller {
 
@@ -13,14 +16,13 @@ public class Controller {
 	private LojaRepository loja;
 
 	@GetMapping("/aff")
-	private String aff() {
+	private Map<Integer, String> aff() {
 		Loja loja1 = new Loja("Loja 1 né pai", "Uma rua atras daquela rua la");
 		Loja loja2 = new Loja("Loja 2 grazadeus", "Aquela rua la");
 		loja.save(loja1);
 		loja.save(loja2);
-		StringBuilder text = new StringBuilder();
-		loja.findAll().stream().map(Loja::getNome).forEach(s -> text.append(s).append(" | "));
-		String ret = text.toString();
-		return ret.trim().substring(0, ret.length() - 2).trim();
+		Map<Integer, String> ret = new HashMap<>();
+		loja.findAll().forEach(l -> ret.put(l.getLojaId(), l.getNome()));
+		return ret;
 	}
 }
