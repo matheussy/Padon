b23b37a936d4b7e8eb36a19788e9d71076a8d69e
Matheus Oliveira <matheus.oliveira.2019@outlook.com>
2023-10-29 10:21:39 -0300

    resto dos repos

---
+++ backend/src/main/java/br/com/padon/application/repositorys/ConterRepository.java
@@ -0,0 +1,8 @@
+package br.com.padon.application.repositorys;
+
+import br.com.padon.application.models.Conter;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConterRepository extends JpaRepository<Conter, Integer> {
+
+}
---
+++ backend/src/main/java/br/com/padon/application/repositorys/ForneceRepository.java
@@ -0,0 +1,8 @@
+package br.com.padon.application.repositorys;
+
+import br.com.padon.application.models.Fornece;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ForneceRepository extends JpaRepository<Fornece, Integer> {
+
+}
