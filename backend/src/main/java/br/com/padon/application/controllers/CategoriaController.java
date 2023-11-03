06dfb19029282cd18aceb95b71b991a914a63f4c
Matheus Oliveira <matheus.oliveira.2019@outlook.com>
2023-11-03 16:00:31 -0300

    categoria controller

--- backend/src/main/java/br/com/padon/application/controllers/CategoriaController.java
+++ backend/src/main/java/br/com/padon/application/controllers/CategoriaController.java
@@ -3,17 +3,40 @@ package br.com.padon.application.controllers;
 import br.com.padon.application.models.Categoria;
 import br.com.padon.application.repositorys.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class CategoriaController {
 
-    @Autowired
-    private CategoriaRepository categoria;
+	@Autowired
+	private CategoriaRepository categoria;
 
-    @PostMapping("/categoria/create")
-    public void createCategoria(@RequestParam(name = "name") String name, @RequestParam(name = "description") String description) {
-        categoria.save(new Categoria(name, description));
-    }
+	@PostMapping("/categoria/create")
+	public void createCategoria(@RequestParam(name = "nome") String nome, @RequestParam(name = "descricao") String descricao) {
+		categoria.save(new Categoria(nome, descricao));
+	}
 
+	@GetMapping("/categoria/get")
+	public List<Categoria> getAllCategorias() {
+		return categoria.findAll();
+	}
+
+	@PostMapping("/categoria/save")
+	public void saveCategoria(@RequestParam(name = "id") int categoriaId, @RequestParam(name = "nome") String nome, @RequestParam(name = "descricao") String descricao) {
+		Categoria categoriaById = categoria.findById(categoriaId).orElseThrow();
+		categoriaById.setNome(nome);
+		categoriaById.setDescricao(descricao);
+		categoria.save(categoriaById);
+	}
+
+	@PostMapping("/categoria/byid")
+	public Optional<Categoria> saveCategoria(@RequestParam(name = "id") int categoriaId) {
+		return categoria.findById(categoriaId);
+	}
 }
--- backend/src/main/java/br/com/padon/application/models/Categoria.java
+++ backend/src/main/java/br/com/padon/application/models/Categoria.java
@@ -12,6 +12,10 @@ public class Categoria {
 	private String nome;
 	private String descricao;
 
+	public Categoria() {
+
+	}
+
 	public Categoria(String nome, String descricao) {
 		this.nome = nome;
 		this.descricao = descricao;
