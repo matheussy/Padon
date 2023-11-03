e00d22700f69de035676ee6e79f7cc415fee832b
Matheus Oliveira <matheus.oliveira.2019@outlook.com>
2023-11-03 17:24:34 -0300

    vixe

--- backend/src/main/java/br/com/padon/application/models/Trabalha.java
+++ backend/src/main/java/br/com/padon/application/models/Trabalha.java
@@ -8,17 +8,13 @@ public class Trabalha {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE)
-	private int participaId;
+	private int trabalhaId;
 	private final int lojaId;
 	private final String cpf;
 
 	public Trabalha(int lojaId, String cpf) {
 		this.lojaId = lojaId;
 		this.cpf = cpf;
-	}
-
-	public int getParticipaId() {
-		return participaId;
 	}
 
 	public int getLojaId() {
