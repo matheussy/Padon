e480d436235be58f7cd3dd33f1c5ecf11e692e8a
Matheus Oliveira <matheus.oliveira@sentry.com.br>
2023-11-13 17:48:01 -0300

    aff

--- padonfrontend/src/pages/Home.js
+++ padonfrontend/src/pages/Home.js
@@ -5,6 +5,18 @@ import reportWebVitals from '../reportWebVitals';*/
 
 export default function Home() {
   return (
-    <p>Teste Home</p>
+    <div className='justify-content-center row'>
+      <div className='col-11 col-md-9'>
+        <div className='card mt-1'>
+          <div className='card-header text-center'>
+            <span className='h4'>Home</span>
+          </div>
+          <div className='card-body'>
+            <div className='m-2'>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
