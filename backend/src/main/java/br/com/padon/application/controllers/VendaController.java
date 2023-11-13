cf01f9a5fcc30cff424d5889f2074d20bcaf8c13
Matheus Oliveira <matheus.oliveira@sentry.com.br>
2023-11-13 16:07:14 -0300

    aff_fofs_fds

---
+++ backend/src/main/java/br/com/padon/application/controllers/VendaController.java
@@ -0,0 +1,103 @@
+package br.com.padon.application.controllers;
+
+import br.com.padon.application.models.Participa;
+import br.com.padon.application.models.Pertence;
+import br.com.padon.application.models.Venda;
+import br.com.padon.application.repositorys.PertenceRepository;
+import br.com.padon.application.repositorys.VendaRepository;
+import com.fasterxml.jackson.databind.JsonNode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/venda")
+public class VendaController {
+
+	@Autowired
+	private VendaRepository venda;
+	@Autowired
+	private PertenceRepository pertence;
+
+	@PostMapping("/create")
+	public Venda createVenda(@RequestBody JsonNode node) throws ParseException {
+		return venda.save(new Venda(
+				DateFormat.getDateInstance().parse(node.get("data").asText()),
+				node.get("status").asText(),
+				node.get("valor").asDouble(),
+				node.get("comanda").asInt()
+		));
+	}
+
+	@GetMapping("/get")
+	public List<Venda> getAllVendas() {
+		return venda.findAll();
+	}
+
+	@PostMapping("/save")
+	public Venda saveVenda(@RequestBody JsonNode node) throws ParseException {
+		Venda vendaById = venda.findById(node.get("id").asInt()).orElseThrow();
+		if (vendaById.getComanda() == 0) {
+			throw new IllegalAccessError("comanda == 0, alteração negada");
+		}
+		vendaById.setDataVenda(DateFormat.getDateInstance().parse(node.get("data").asText()));
+		vendaById.setStatusVenda(node.get("status").asText());
+		vendaById.setValorTotal(node.get("valor").asDouble());
+		vendaById.setComanda(node.get("comanda").asInt());
+		return venda.save(vendaById);
+	}
+
+	@PostMapping("/byid")
+	public Optional<Venda> getVenda(@RequestBody JsonNode node) {
+		return venda.findById(node.get("id").asInt());
+	}
+
+	@PostMapping("/delete")
+	public boolean deleteVenda(@RequestBody JsonNode node) {
+		Venda vendaById = venda.findById(node.get("id").asInt()).orElseThrow();
+		if (vendaById.getComanda() == 0) {
+			throw new IllegalAccessError("comanda == 0, alteração negada");
+		}
+		venda.delete(vendaById);
+		return true;
+	}
+
+	@PostMapping("/add")
+	public Pertence addProduto(@RequestBody JsonNode node) {
+		int vendaId = node.get("vendaId").asInt();
+		Venda vendaById = venda.findById(vendaId).orElseThrow();
+		if (vendaById.getComanda() == 0) {
+			throw new IllegalAccessError("comanda == 0, alteração negada");
+		}
+
+		return pertence.save(new Pertence(
+				node.get("produtoId").asInt(),
+				vendaId,
+				node.get("precoTotal").asDouble(),
+				node.get("quantidade").asInt(),
+				node.get("precoAtual").asDouble()
+		));
+	}
+
+	@PostMapping("/remove")
+	public boolean removeProduto(@RequestBody JsonNode node) {
+		int vendaId = node.get("vendaId").asInt();
+		Venda vendaById = venda.findById(vendaId).orElseThrow();
+		if (vendaById.getComanda() == 0) {
+			throw new IllegalAccessError("comanda == 0, alteração negada");
+		}
+
+		Pertence result = pertence.getPertence(node.get("produtoId").asInt(), vendaId);
+		if (result == null) {
+			return false;
+		}
+
+		pertence.delete(result);
+		return true;
+	}
+}
--- backend/src/main/java/br/com/padon/application/repositorys/PertenceRepository.java
+++ backend/src/main/java/br/com/padon/application/repositorys/PertenceRepository.java
@@ -2,7 +2,10 @@ package br.com.padon.application.repositorys;
 
 import br.com.padon.application.models.Pertence;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PertenceRepository extends JpaRepository<Pertence, Integer> {
 
+	@Query("select p from Pertence p where p.produtoId = :produtoId and p.vendaId = :vendaId")
+	Pertence getPertence(int produtoId, int vendaId);
 }
