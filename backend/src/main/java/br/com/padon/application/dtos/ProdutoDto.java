41094a12cf8fc94e161119792d34f0a41a3b5d9a
Matheus Oliveira <matheus.oliveira@sentry.com.br>
2023-11-13 10:56:13 -0300

    fofss

--- backend/src/main/java/br/com/padon/application/dtos/ProdutoDto.java
+++ backend/src/main/java/br/com/padon/application/dtos/ProdutoDto.java
@@ -1,5 +1,15 @@
 package br.com.padon.application.dtos;
 
-public record ProdutoDto(int produtoId, String nome, String fabricante, String image, double codigoDeBarras, boolean bloqueado, double precoPorQuilo, double precoPorUnidade, boolean porQuilo, double preco) {
-
+public record ProdutoDto(
+		int produtoId,
+		String nome,
+		String fabricante,
+		String image,
+		double codigoDeBarras,
+		boolean bloqueado,
+		double precoPorQuilo,
+		double precoPorUnidade,
+		boolean porQuilo,
+		double preco
+) {
 }
