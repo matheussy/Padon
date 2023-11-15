741e03cb88201262415f5b691d79e74958aa5879
Matheus Oliveira <matheus.oliveira.2019@outlook.com>
2023-11-15 18:17:25 -0300

    agr foi mano na moral

--- backend/src/main/java/br/com/padon/application/security/SecurityService.java
+++ backend/src/main/java/br/com/padon/application/security/SecurityService.java
@@ -32,7 +32,6 @@ public class SecurityService {
 	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 		return httpSecurity
 				.csrf(AbstractHttpConfigurer::disable)
-				.cors(AbstractHttpConfigurer::disable)
 				.sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 				.authorizeHttpRequests(a -> a
 						.requestMatchers(HttpMethod.POST, "/login").permitAll()
