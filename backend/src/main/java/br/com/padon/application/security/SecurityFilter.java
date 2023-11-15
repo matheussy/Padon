e3edc2fa00397f2e50f791dfdc84fe57a6947b14
Matheus Oliveira <matheus.oliveira.2019@outlook.com>
2023-11-15 17:59:40 -0300

    foi

--- backend/src/main/java/br/com/padon/application/security/SecurityFilter.java
+++ backend/src/main/java/br/com/padon/application/security/SecurityFilter.java
@@ -35,7 +35,7 @@ public class SecurityFilter extends OncePerRequestFilter {
 
 	private String recoverToken(HttpServletRequest request) {
 		var authHeader = request.getHeader("token");
-		if (authHeader == null) return null;
+		if (authHeader == null || authHeader.isBlank()) return null;
 		return authHeader.replace("Bearer ", "");
 	}
 }
