1dfa66441b33742a479e88b50d3d0504054d2d9f
Matheus Oliveira <matheus.oliveira.2019@outlook.com>
2023-11-15 17:37:00 -0300

    putz

--- backend/src/main/java/br/com/padon/application/controllers/LoginController.java
+++ backend/src/main/java/br/com/padon/application/controllers/LoginController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.HashMap;
 import java.util.Map;
 
-
+@CrossOrigin
 @RestController
 public class LoginController {
 
